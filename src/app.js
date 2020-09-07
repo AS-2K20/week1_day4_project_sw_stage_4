10d4944e70674650dbc16f5fa3cac8dff5068596
AS-2K20 <amudhan.psgcas@gmail.com>
2020-09-07 16:11:52 +0530

    Replacing Loops with Map,Filter,Join

--- src/app.js
+++ src/app.js
@@ -26,7 +26,12 @@ class Player {
     constructor(id, name, type) {
         // Create member variables and assign values
         // Type your code
-
+        this.id=id;
+        this.name=name;
+        this.strength=this.getRandomStrength();
+        this.image="images/super-"+(id+1)+".png";
+        console.log(this.image);
+        this.type=type;
     }
 
     // getting random strength
@@ -36,10 +41,20 @@ class Player {
 
     // Create a player for displaying
     view = () => {
-        // Accumulate HTML template
-        // Type your code here
-
-    
+        let player= document.createElement('div');
+        player.classList.add('player');
+        player.setAttribute('data-id',this.id);
+        if(this.selected==true){
+            player.classList.add('selected');
+        }
+        let image= document.createElement('img');
+        image.setAttribute('src',this.image);
+        let name=document.createElement('div');
+        name.textContent=this.name;
+        let strength=document.createElement('div');
+        strength.textContent=this.strength;
+        strength.className='strength';
+        player.append(image,name,strength);    
         return player;
     }
 }
@@ -50,7 +65,17 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
-
+    this.players=players.map((player,i)=>{
+        let type = (i%2==0)?'hero':'villain';
+        return new Player(i,player,type);
+    });
+    this.score=[0,0];
+    Array.from(document.getElementsByClassName('team'))
+    .forEach(elem=>elem
+        .addEventListener('click',(e)=>{
+            this.handleSelection(e.target);
+        }
+        ));
     }
 
     // Display players in HTML
